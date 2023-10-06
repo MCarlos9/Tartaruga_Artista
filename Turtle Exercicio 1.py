6e2fb3701c1641bfff749e1b8e56c04973b11db8
MCarlos9 <133662383+MCarlos9@users.noreply.github.com>
2023-10-06 15:36:56 -0300

    Add files via upload

---
+++ Turtle Exercicio 1.py
@@ -0,0 +1,23 @@
+"""
+Exercícios
+1) Reduza as duas linhas com forward para apenas uma, mas que faça a mesma
+coisa que as atuais
+2) Faça a tartaruga virar para a direita
+3) Altere o comprimento dos lados do quadrado
+4) Transforme o quadrado em um retângulo 
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.shape('turtle')
+
+
+turtle.forward(100) #Linhas reduzidas  #Comprimento alterado 
+turtle.right(90) #Direção alterada
+turtle.forward(150) #Linhas reduzidas  #Comprimento alterado 100-->150   #Formato Retangular
+turtle.right(90) #Direção alterada
+turtle.forward(100) #Linhas reduzidas  #Comprimento alterado
+turtle.right(90) #Direção alterada
+turtle.forward(150) #Linhas reduzidas  #Comprimento alterado 100-->150   #Formato Retangular
+turtle.right(90) #Direção alterada
