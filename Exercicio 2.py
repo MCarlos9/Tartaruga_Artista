8b5d5fd2f406d6b2d31bb68bfdfcd0f1b8a3ced5
MCarlos9 <133662383+MCarlos9@users.noreply.github.com>
2023-10-06 15:37:16 -0300

    Add files via upload

---
+++ Exercicio 2.py
@@ -0,0 +1,22 @@
+"""
+Exercícios
+
+1) Mude a cor da linha
+2) Mude o formato da tartaruga
+3) Mude o tamanho do quadrado
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.shape('circle') #Formato da Tartaruga
+
+turtle.color('cyan') #Cor da linha
+
+turtle.forward(200) #Tamanho alterado 100-->200
+turtle.left(90)
+turtle.forward(200) #Tamanho alterado 100-->200
+turtle.left(90)
+turtle.forward(200) #Tamanho alterado 100-->200
+turtle.left(90)
+turtle.forward(200) #Tamanho alterado 100-->200
